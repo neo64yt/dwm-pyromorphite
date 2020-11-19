8bd47f8dc99c6c5d4e2b702b31aedd6e8db67cc2
lazyneo-setups64 <naufalramli06@gmail.com>
2020-11-19 10:55:26 +0800

    Second commit

--- dwm.c
+++ dwm.c
@@ -272,7 +272,7 @@ static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void zoom(const Arg *arg);
 
 /* variables */
-static Client *wintosystrayicon(Window w);
+static Systray *systray =  NULL;
 static const char autostartblocksh[] = "autostart_blocking.sh";
 static const char autostartsh[] = "autostart.sh";
 static const char broken[] = "broken";
