b67b46a4230b60b53459012933ddfb06797d1f66
lazyneo-setups64 <naufalramli06@gmail.com>
2020-11-21 20:29:14 +0800

    Showing 6 tags only

--- config.def.h
+++ config.def.h
@@ -28,8 +28,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { " 󰎦 ", " 󰎩 ", " 󰎬 ", " 󰎮 ", " 󰎰 ", " 󰎵 ", " 󰎸 ", " 󰎻 ", " 󰎾 " };
-static const char *tagsalt[] = { "[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]", "[9]" };
+static const char *tags[] = { " 󰎦 ", " 󰎩 ", " 󰎬 ", " 󰎮 ", " 󰎰 ", " 󰎵 " };
+static const char *tagsalt[] = { "[1]", "[2]", "[3]", "[4]", "[5]", "[6]" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -101,9 +101,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
 	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} }, 
 };
