5a401dd1ed61b8aef609fa931748ddb0deb8d608
lazyneo-setups64 <naufalramli06@gmail.com>
2020-12-17 21:08:38 +0800

    Window rules for Krita

--- config.def.h
+++ config.def.h
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
+	{ "Krita",     NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
