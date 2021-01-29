fb6fac78fa0497365394c89366688ebfc1f82768
lazyneo-setups64 <naufalramli06@gmail.com>
2021-01-29 21:34:28 +0800

    Using Alacritty

--- config.def.h
+++ config.def.h
@@ -38,7 +38,7 @@ static const char *tagsalt[] = { " 󰎦 ", " 󰎩 ", " 󰎬 ", " 󰎮 ", " 󰎰 
 
 /* Default apps (Please input the window class or the window title of the app, can be determined using xprop) */
 static const char web_browser[] = "Chromium";
-static const char terminal[] = "Termite";
+static const char terminal[] = "Alacritty";
 static const char img_editor[] = "Gimp";
 
 /* Window rules */
